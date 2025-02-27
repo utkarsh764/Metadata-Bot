2dde19c703b0341278180892035e0c000d5b5e79
Toppers-Renamer <renamer.robot@gmail.com>
2025-02-28 02:08:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ class Config(object):
         admin) else admin for admin in os.environ.get('ADMIN', '8181241262').split()]  # ⚠️ Required
     
     FORCE_SUB = os.environ.get("FORCE_SUB", "supertopperschannel") # ⚠️ Required Username without @
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))  # ⚠️ Required
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002424733644"))  # ⚠️ Required
     FLOOD = int(os.environ.get("FLOOD", '10'))
     BANNED_USERS = set(int(x) for x in os.environ.get(
         "BANNED_USERS", "1234567890").split())
